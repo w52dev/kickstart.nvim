9a63a41f2d11ffb7e2878af8bdd3e8d8a4ba4c5b
Cosmin L. Neagu <cln@h2.io>
2024-12-23 13:11:53 +0000

    floating toggleterm

--- init.lua
+++ init.lua
@@ -233,7 +233,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  { 'akinsho/toggleterm.nvim', version = '*', opts = {} },
+  { 'akinsho/toggleterm.nvim', version = '*', opts = { direction = 'float' } },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
